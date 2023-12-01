e8a6f75c28fb567a03a7221fc06b1d77e1ab434b
Rareș Cosma <rares@getbetter.ro>
2023-12-01 13:44:13 +0200

    avoid allocations

--- 2023/src/bin/day01.rs
+++ 2023/src/bin/day01.rs
@@ -2,25 +2,24 @@ use aoc_prelude::{lazy_static, HashMap};
 
 lazy_static! {
     static ref FORWARD_MAP: HashMap<String, String> = {
-        let mut res: HashMap<String, String> = HashMap::new();
+        let mut res = HashMap::new();
         for (idx, digit) in [
             "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
         ]
         .iter()
         .enumerate()
         {
-            let c = digit.chars().collect::<Vec<_>>();
+            let mut c = digit.chars();
             res.insert(
                 digit.to_string(),
-                format!("{}{}{}", c[0], idx + 1, c[c.len() - 1]),
+                format!("{}{}{}", c.next().unwrap(), idx + 1, c.last().unwrap()),
             );
         }
         res
     };
 }
 
-#[inline]
-fn process_line_p1<S: AsRef<str>>(s: S) -> u32 {
+fn process_line<S: AsRef<str>>(s: S) -> u32 {
     let s = s.as_ref().chars();
 
     let first = s.clone().find(|x| x.is_ascii_digit()).unwrap_or('0');
@@ -38,18 +37,17 @@ fn replace_digits<S: AsRef<str>>(s: S) -> String {
     s
 }
 
-fn read_input() -> Vec<String> {
+fn read_input() -> Vec<&'static str> {
     include_str!("../../inputs/day01.txt")
         .lines()
-        .map(String::from)
         .collect::<Vec<_>>()
 }
 
 aoc_2023::main! {
     let input = read_input();
 
-    let p1: u32 = input.iter().map(process_line_p1).sum();
-    let p2: u32 = input.iter().map(replace_digits).map(process_line_p1).sum();
+    let p1: u32 = input.iter().map(process_line).sum();
+    let p2: u32 = input.iter().map(replace_digits).map(process_line).sum();
 
     (p1, p2)
 }
