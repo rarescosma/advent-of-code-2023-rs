21969f3d1a01eb934862049ac66ff0d2a12bee71
Rareș Cosma <rares@getbetter.ro>
2023-12-14 18:57:49 +0200

    day 14: try out const generics

--- 2023/src/bin/day14.rs
+++ 2023/src/bin/day14.rs
@@ -1,89 +1,14 @@
 use ahash::RandomState;
-use aoc_2dmap::prelude::{Map, Pos};
-use aoc_prelude::{lazy_static, HashSet};
-use std::hash::BuildHasher;
-use std::hash::{Hash, Hasher};
-use std::ops::RangeInclusive;
 
-const MAP_SIZE: i32 = 100;
-const NORTH: usize = 0;
-const WEST: usize = 1;
-const SOUTH: usize = 2;
-const EAST: usize = 3;
+use aoc_prelude::{lazy_static, HashSet, Itertools};
+use std::fmt::{Display, Formatter};
+use std::hash::{BuildHasher, Hash, Hasher};
+
+use std::ptr;
+use std::str::FromStr;
 
 lazy_static! {
-    static ref OFFSET: [Pos; 4] = [
-        Pos::from((0, -1)),
-        Pos::from((-1, 0)),
-        Pos::from((0, 1)),
-        Pos::from((1, 0)),
-    ];
-    // (from row/col to row/col, reverse)
-    static ref TILT: [(RangeInclusive<i32>, bool); 4] = [
-        (1..=MAP_SIZE - 1, false),
-        (1..=MAP_SIZE - 1, false),
-        (0..=MAP_SIZE - 2, true),
-        (0..=MAP_SIZE - 2, true),
-    ];
     static ref HASHER_BUILDER: RandomState = RandomState::new();
-}
-
-fn make_pos(c1: i32, c2: i32, dir: usize) -> Pos {
-    match dir {
-        NORTH | SOUTH => (c2, c1).into(),
-        EAST | WEST => (c1, c2).into(),
-        _ => unreachable!(),
-    }
-}
-
-fn cast_ray(p: Pos, m: &Map<char>, dir: usize) -> Pos {
-    let mut ret = p;
-    let offset = OFFSET[dir];
-    loop {
-        let probe = ret + offset;
-        if m.get(probe) == Some('.') {
-            ret = probe;
-        } else {
-            break;
-        }
-    }
-    ret
-}
-
-fn tilt(m: &mut Map<char>, dir: usize) {
-    let (mut rng, rev) = TILT[dir].clone();
-    let it = if rev {
-        RangeInclusive::next_back
-    } else {
-        RangeInclusive::next
-    };
-    while let Some(c1) = it(&mut rng) {
-        for c2 in 0..MAP_SIZE {
-            let p = make_pos(c1, c2, dir);
-            if m.get_unchecked(p) == 'O' {
-                let new_pos = cast_ray(p, m, dir);
-                if new_pos != p {
-                    m.swap(new_pos, p);
-                }
-            }
-        }
-    }
-}
-
-fn load(m: &Map<char>) -> i32 {
-    m.iter()
-        .filter_map(|p| {
-            if m.get_unchecked(p) == 'O' {
-                Some(m.size.y - p.y)
-            } else {
-                None
-            }
-        })
-        .sum::<i32>()
-}
-
-fn cycle(m: &mut Map<char>) {
-    (0..=3).for_each(|dir| tilt(m, dir));
 }
 
 fn manually_hash<H: Hash>(state: &H) -> u64 {
@@ -92,34 +17,34 @@ fn manually_hash<H: Hash>(state: &H) -> u64 {
     hasher.finish()
 }
 
-fn multicycle<T: Clone + Eq + PartialEq + Hash, F: Fn(&mut T), U: Copy, S: Fn(&T) -> U>(
+fn multicycle<T: Clone + Eq + PartialEq + Hash, F: Fn(&mut T)>(
     m: T,
     cycle_f: F,
-    score_f: S,
     num_cycles: usize,
-) -> U {
+) -> T {
     assert!(num_cycles > 0);
 
     let mut cache: HashSet<u64> = HashSet::with_capacity(512);
-    let mut queue: Vec<(u64, U)> = Vec::with_capacity(512);
+    let mut queue: Vec<(u64, T)> = Vec::with_capacity(512);
     let mut look_for = None;
 
-    let m = &mut m.clone();
+    let m = m.clone();
+    let mc = &mut m.clone();
 
     let cycle_after = (0..num_cycles).find(|_| {
-        let h = manually_hash(m);
+        let h = manually_hash(mc);
         if cache.contains(&h) {
             look_for = Some(h);
             true
         } else {
             cache.insert(h);
-            queue.push((h, score_f(m)));
-            cycle_f(m);
+            queue.push((h, mc.clone()));
+            cycle_f(mc);
             false
         }
     });
     if cycle_after.is_none() {
-        return score_f(m);
+        return m;
     }
 
     let cycle_after = cycle_after.unwrap();
@@ -135,22 +60,122 @@ fn multicycle<T: Clone + Eq + PartialEq + Hash, F: Fn(&mut T), U: Copy, S: Fn(&T
     queue.remove(idx).1
 }
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+struct FooMap<const M: usize> {
+    inner: [[char; M]; M],
+}
+
+impl<const M: usize> Display for FooMap<M> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(
+            &self
+                .inner
+                .iter()
+                .map(|x| x.iter().collect::<String>())
+                .join("\n"),
+        )
+    }
+}
+
+impl<const M: usize> FromStr for FooMap<M> {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut inner = [[' '; M]; M];
+        for (i, c) in s[0..M * M].chars().enumerate() {
+            inner[i / M][i % M] = c;
+        }
+        Ok(FooMap { inner })
+    }
+}
+
+impl<const M: usize> FooMap<M> {
+    fn transpose(&mut self) {
+        for r in 0..M {
+            for c in r..M {
+                // trust me
+                if c != r {
+                    unsafe {
+                        ptr::swap(&mut self.inner[r][c], &mut self.inner[c][r]);
+                    }
+                }
+            }
+        }
+    }
+
+    fn flip_vertical(&mut self) {
+        for row in &mut self.inner {
+            row.reverse();
+        }
+    }
+
+    fn tilt_left(&mut self) {
+        let mut bins = [0; M];
+
+        for row in self.inner.as_mut() {
+            let mut i = 0;
+
+            for (idx, &c) in row.iter().enumerate().skip(1) {
+                if c == 'O' {
+                    bins[i] = idx;
+                    i += 1;
+                }
+            }
+            for &pos in &bins[0..i] {
+                let mut new_idx = pos;
+                for inner in (0..pos).rev() {
+                    if row[inner] != '.' {
+                        break;
+                    } else {
+                        new_idx = inner;
+                    }
+                }
+                // trust me
+                if new_idx != pos {
+                    unsafe {
+                        ptr::swap(&mut row[pos], &mut row[new_idx]);
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn const_cycle<const M: usize>(m: &mut FooMap<M>) {
+    for _ in 0..4 {
+        m.transpose();
+        m.tilt_left();
+        m.flip_vertical();
+    }
+}
+
+fn const_load<const M: usize>(m: &FooMap<M>) -> i32 {
+    let mut ans = 0;
+    for (r, row) in m.inner.into_iter().enumerate() {
+        for el in row.into_iter() {
+            if el == 'O' {
+                ans += (M - r) as i32;
+            }
+        }
+    }
+    ans
+}
+
 fn solve() -> (i32, i32) {
-    let input = include_str!("../../inputs/day14.txt")
-        .lines()
-        .collect::<Vec<_>>();
+    let c_map = include_str!("../../inputs/day14.txt")
+        .replace('\n', "")
+        .trim()
+        .parse::<FooMap<100>>()
+        .expect("nope");
 
-    let mut p1_map = Map::new(
-        (input[0].len(), input.len()),
-        input.into_iter().flat_map(|x| x.chars()),
-    );
-    let p2_map = p1_map.clone();
+    let mut p1_map = c_map;
+    p1_map.transpose();
+    p1_map.tilt_left();
+    p1_map.transpose();
+    let p1 = const_load(&p1_map);
 
-    tilt(&mut p1_map, NORTH);
-
-    let p2 = multicycle(p2_map, cycle, load, 1000000000);
-
-    (load(&p1_map), p2)
+    let p2 = const_load(&multicycle(c_map, const_cycle, 1000000000));
+    (p1, p2)
 }
 
 aoc_2023::main! {
