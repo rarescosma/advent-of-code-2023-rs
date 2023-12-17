911d184acf52f0c2f71ea4e51681b6b4ffb53041
Rareș Cosma <rares@getbetter.ro>
2023-12-17 11:46:15 +0200

    day 17: nits

--- 2023/src/bin/day17.rs
+++ 2023/src/bin/day17.rs
@@ -4,12 +4,20 @@ use aoc_prelude::{lazy_static, ArrayVec};
 use std::hash::Hash;
 
 lazy_static! {
-    // North, South, East, West
-    static ref OFFSETS: [Pos; 4] = [(0, -1).into(), (1, 0).into(), (0, 1).into(), (-1, 0).into()];
+    // West, North, East, South
+    // 00, 01, 10, 11
+    static ref OFFSETS: [Pos; 4] = [(-1, 0).into(), (0, -1).into(), (1, 0).into(), (0, 1).into()];
 }
 
 fn is_opposite(dir: usize, to: usize) -> bool {
     (dir + 2) % 4 == to
+}
+
+fn pos_to_dir(p: Pos) -> usize {
+    let signum = p.x + p.y;
+    let msb = if signum < 0 { 0 } else { 1 };
+    let lsb = if p.y == 0 { 0 } else { 1 };
+    lsb + (msb << 1)
 }
 
 #[derive(PartialOrd, Ord, Eq, PartialEq, Hash, Clone, Copy)]
@@ -22,7 +30,7 @@ struct State {
 
 struct Move {
     to: Pos,
-    cost: u32,
+    cost: usize,
 }
 
 impl GameState<LavaCtx> for State {
@@ -33,59 +41,51 @@ impl GameState<LavaCtx> for State {
     }
 
     fn steps(&self, ctx: &mut LavaCtx) -> Self::Steps {
-        if self.direction.is_none() {
-            return self
-                .cur
-                .neighbors_simple()
-                .filter_map(|to| ctx.map.get(to).map(|cost| Move { to, cost }))
-                .collect();
-        }
-
-        let direction = self.direction.unwrap();
-
+        let make_move = |(new_direction, to)| {
+            ctx.map.get(to).and_then(|cost| match self.direction {
+                None => Some(Move { to, cost }),
+                Some(direction)
+                    if new_direction == direction && self.went_straight < ctx.max_straight =>
+                {
+                    Some(Move { to, cost })
+                }
+                Some(direction)
+                    if new_direction != direction
+                        && !is_opposite(new_direction, direction)
+                        && ctx.ultra_check(self.went_straight) =>
+                {
+                    Some(Move { to, cost })
+                }
+                _ => None,
+            })
+        };
         OFFSETS
             .into_iter()
             .map(|o| self.cur + o)
             .enumerate()
-            .filter_map(|(new_direction, to)| {
-                ctx.map.get(to).and_then(|cost| {
-                    if new_direction == direction && self.went_straight < ctx.max_straight {
-                        return Some(Move { to, cost });
-                    }
-                    if new_direction != direction
-                        && !is_opposite(new_direction, direction)
-                        && ctx.ultra_check(self.went_straight)
-                    {
-                        return Some(Move { to, cost });
-                    }
-                    None
-                })
-            })
+            .filter_map(make_move)
             .collect()
     }
 }
 
 impl Transform<State> for Move {
     fn cost(&self) -> usize {
-        self.cost as usize
+        self.cost
     }
 
-    fn transform(&self, game_state: &State) -> State {
-        let offset = self.to - game_state.cur;
-        let new_direction = OFFSETS.into_iter().position(|x| x == offset);
+    fn transform(&self, state: &State) -> State {
+        let new_direction = Some(pos_to_dir(self.to - state.cur));
 
-        let went_straight =
-            1 + if game_state.direction.is_none() || new_direction == game_state.direction {
-                // continue if direction matches (or we're at the beginning)
-                game_state.went_straight
-            } else {
-                // reset if direction changed
-                0
-            };
+        let went_straight = 1 + if new_direction == state.direction {
+            state.went_straight
+        } else {
+            // None branch corresponds to initial state so 0 is fine
+            0
+        };
 
         State {
             cur: self.to,
-            goal: game_state.goal,
+            goal: state.goal,
             direction: new_direction,
             went_straight,
         }
@@ -93,7 +93,7 @@ impl Transform<State> for Move {
 }
 
 struct LavaCtx {
-    map: Map<u32>,
+    map: Map<usize>,
     max_straight: usize,
     min_straight: Option<usize>,
 }
@@ -116,7 +116,10 @@ fn solve() -> (usize, usize) {
 
     let map = Map::new(
         size,
-        input.join("").chars().map(|c| c.to_digit(10).unwrap()),
+        input
+            .join("")
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as usize),
     );
     let init_state = State {
         cur: Pos::default(),
