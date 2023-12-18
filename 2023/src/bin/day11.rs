f7e52bef01dc7ed285848a3696b2ae4fb1362fbe
Rareș Cosma <rares@getbetter.ro>
2023-12-18 15:06:57 +0200

    day 11: optimize to use PSA + O(N) distance calculation

--- 2023/src/bin/day11.rs
+++ 2023/src/bin/day11.rs
@@ -1,76 +1,65 @@
-use aoc_2dmap::prelude::{Map, Pos};
-use aoc_prelude::Itertools;
-use std::cmp::{max, min};
+use aoc_2023::ConstMap;
+use aoc_prelude::PrimInt;
+use std::ops::AddAssign;
 
-fn find_empty_lines<S: AsRef<str>>(lines: impl Iterator<Item = S>) -> Vec<i32> {
-    lines
-        .filter(|x| !x.as_ref().is_empty())
-        .enumerate()
-        .filter_map(|(idx, content)| {
-            if content.as_ref().chars().all(|c| c == '.') {
-                Some(idx as i32)
-            } else {
-                None
-            }
-        })
-        .sorted()
-        .collect::<Vec<_>>()
-}
-
-fn manhattan(p: Pos, q: Pos) -> usize {
-    let m = p - q;
-    (m.x.abs() + m.y.abs()) as usize
-}
-
-fn solve() -> (usize, usize) {
-    let input = include_str!("../../inputs/day11.txt")
-        .lines()
-        .collect::<Vec<_>>();
-
-    // get the coords of empty lines => store them as cols because we transpose
-    let empty_cols = find_empty_lines(input.iter());
-
-    // poor man's transpose, but we also get a map
-    let w = input[0].len();
-    let h = input.len();
-
-    let mut transposed_chars = vec![' '; w * h];
-    for y in 0..h {
-        for (x, c) in input[y].chars().enumerate() {
-            transposed_chars[x * h + y] = c;
+fn psa_elements<'a, const M: usize, P: PrimInt + 'a>(
+    c_map: &'a ConstMap<M>,
+    scale: P,
+) -> impl Iterator<Item = P> + 'a {
+    c_map.inner.iter().map(move |content| {
+        if content.iter().all(|c| c == &'.') {
+            scale
+        } else {
+            P::from(1).unwrap()
         }
+    })
+}
+
+fn make_psa<const M: usize, P: PrimInt>(xs: impl Iterator<Item = P>) -> [P; M] {
+    let mut res = [P::from(0).unwrap(); M];
+    let mut l_sum = P::from(0).unwrap();
+    for (i, x) in xs.enumerate() {
+        let n_sum = l_sum + x;
+        res[i] = n_sum;
+        l_sum = n_sum;
     }
-    let map = Map::new((h, w), transposed_chars.into_iter());
+    res
+}
 
-    // get the coords of empty lines => store them as rows
-    let empty_rows = find_empty_lines(format!("{map}").lines());
+fn total_distance<const M: usize, P: PrimInt + AddAssign>(c_map: &ConstMap<M>, scale: P) -> P {
+    let psa = make_psa::<M, P>(psa_elements(c_map, scale));
 
-    let pairs = map
+    let mut total = P::from(0).unwrap();
+    let mut num_seen = P::from(0).unwrap();
+    let mut cumulative = P::from(0).unwrap();
+    c_map
+        .inner
         .iter()
-        .filter(|p| map.get_unchecked(p) == '#')
-        .collect::<Vec<_>>()
-        .into_iter()
-        .tuple_combinations::<(_, _)>()
-        .map(|(g1, g2)| {
-            let min_x = min(g1.x, g2.x);
-            let min_y = min(g1.y, g2.y);
-            let max_x = max(g1.x, g2.x);
-            let max_y = max(g1.y, g2.y);
+        .map(|x| x.iter().filter(|&c| c == &'#').count())
+        .enumerate()
+        .for_each(|(index, count)| {
+            let count_p = P::from(count).unwrap();
+            total += (num_seen * psa[index] - cumulative) * count_p;
+            cumulative += psa[index] * count_p;
+            num_seen += count_p;
+        });
+    total
+}
 
-            let empty_rows_between = empty_rows
-                .iter()
-                .filter(|y| **y > min_y && **y < max_y)
-                .count();
-            let empty_cols_between = empty_cols
-                .iter()
-                .filter(|x| **x > min_x && **x < max_x)
-                .count();
-            (manhattan(g1, g2), empty_rows_between + empty_cols_between)
-        })
-        .collect::<Vec<_>>();
+fn solve() -> (u64, u64) {
+    let mut c_map = include_str!("../../inputs/day11.txt")
+        .replace('\n', "")
+        .trim()
+        .parse::<ConstMap<140>>()
+        .expect("nope");
 
-    let p1 = pairs.iter().map(|&(d, e)| d + e).sum();
-    let p2 = pairs.iter().map(|&(d, e)| d + e * (1000000 - 1)).sum();
+    let p1_rows = total_distance(&c_map, 2);
+    let p2_rows = total_distance(&c_map, 1000000);
+
+    c_map.transpose();
+
+    let p1 = p1_rows + total_distance(&c_map, 2);
+    let p2 = p2_rows + total_distance(&c_map, 1000000);
 
     (p1, p2)
 }
