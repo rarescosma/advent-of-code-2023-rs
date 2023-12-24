a5e5aafe446c7349c39766b996a3cccb001be4e3
Rareș Cosma <rares@getbetter.ro>
2023-12-24 08:12:07 +0200

    day 23: stolen a fine fast 1u64-based BFS from reddit
    along with a min cost guard for recursing,
    ... and we're down to 40ms !

--- 2023/src/bin/day23.rs
+++ 2023/src/bin/day23.rs
@@ -1,5 +1,5 @@
 use aoc_2dmap::prelude::{Map, Pos};
-use aoc_prelude::{lazy_static, ArrayVec, HashMap, HashSet};
+use aoc_prelude::{lazy_static, ArrayVec, Entry, HashMap, HashSet};
 use std::collections::VecDeque;
 use std::iter::once;
 use std::sync::Mutex;
@@ -9,20 +9,11 @@ type Edges = ArrayVec<(Node, usize), 64>;
 type Graph = ArrayVec<Edges, MAX_NODES>;
 
 lazy_static! {
-    static ref COUNTER: Mutex<(HashMap<Pos, usize>, usize)> = Mutex::new((HashMap::new(), 0));
+    static ref ID_MAKER: Mutex<(HashMap<Pos, usize>, usize)> = Mutex::new((HashMap::new(), 0));
 }
 
-fn unique_id<P: AsRef<Pos>>(pos: P) -> usize {
-    let pos = pos.as_ref();
-    let mut guard = COUNTER.lock().unwrap();
-    if guard.0.contains_key(pos) {
-        guard.0[pos]
-    } else {
-        guard.1 += 1;
-        let id = guard.1;
-        guard.0.insert(*pos, id);
-        id
-    }
+fn make_array<const M: usize, T: Default>() -> ArrayVec<T, M> {
+    ArrayVec::<T, M>::from_iter((0..M).map(|_| T::default()))
 }
 
 #[derive(Eq, PartialEq, Ord, PartialOrd, Hash, Copy, Clone, Debug)]
@@ -30,41 +21,118 @@ struct Node {
     id: usize,
 }
 
+impl Node {
+    fn make_id<P: AsRef<Pos>>(pos: P) -> usize {
+        let pos = pos.as_ref();
+        let mut guard = ID_MAKER.lock().unwrap();
+        if guard.0.contains_key(pos) {
+            guard.0[pos]
+        } else {
+            guard.1 += 1;
+            let id = guard.1;
+            guard.0.insert(*pos, id);
+            id
+        }
+    }
+}
+
 impl<P: AsRef<Pos>> From<P> for Node {
     fn from(pos: P) -> Self {
-        Self { id: unique_id(pos) }
+        Self {
+            id: Self::make_id(pos),
+        }
     }
 }
 
 struct World {
     graph: Graph,
+    start: Node,
     goal: Node,
+    adj_masks: ArrayVec<u64, MAX_NODES>,
 }
 
-fn make_array<const M: usize, T: Default>() -> ArrayVec<T, M> {
-    ArrayVec::<T, M>::from_iter((0..M).map(|_| T::default()))
-}
+impl World {
+    fn from_map<M: Fn(Pos) -> ArrayVec<Pos, 4>>(map: &Map<char>, make_neighbors: M) -> Self {
+        // reset the ID maker so we can fit our seen & reachable sets within u64
+        {
+            let mut guard = ID_MAKER.lock().unwrap();
+            guard.0.clear();
+            guard.1 = 0;
+        }
 
-fn make_world<R: Fn(Pos) -> bool, M: Fn(Pos) -> ArrayVec<Pos, 4>>(
-    map: &Map<char>,
-    (start_pos, goal_pos): (Pos, Pos),
-    make_neighbors: M,
-    is_poi: R,
-) -> World {
-    let goal = Node::from(goal_pos);
+        let (start_pos, goal_pos) = (Pos::new(1, 0), map.size - Pos::new(2, 1));
+        let start = Node::from(start_pos);
+        let goal = Node::from(goal_pos);
 
-    let mut graph = make_array::<MAX_NODES, Edges>();
-    let mut q = VecDeque::new();
-    let mut seen = HashSet::new();
+        let mut graph = make_array::<MAX_NODES, Edges>();
+        let mut q = VecDeque::new();
+        let mut seen = HashSet::new();
 
-    for tile in map.iter().filter(|&p| is_poi(p)).chain(once(start_pos)) {
-        q.clear();
-        q.push_back((tile, 0));
-        seen.clear();
-        graph[Node::from(tile).id] = bfs(&mut q, &mut seen, &make_neighbors, &is_poi, map);
+        for tile in map
+            .iter()
+            .filter(|&p| is_intersection(p, map))
+            .chain(once(start_pos))
+            .chain(once(goal_pos))
+        {
+            q.clear();
+            q.push_back((tile, 0));
+            seen.clear();
+
+            let mut res = Edges::new();
+
+            // BFS from this POI to adjacent POIs and stop there
+            while let Some((cur, t)) = q.pop_front() {
+                if seen.contains(&cur) {
+                    continue;
+                }
+                seen.insert(cur);
+
+                for next in make_neighbors(cur)
+                    .into_iter()
+                    .filter(|&p| is_valid(p, map) && !seen.contains(&p))
+                {
+                    if is_intersection(next, map) || next == goal_pos {
+                        res.push((Node::from(next), t + 1));
+                    } else {
+                        q.push_back((next, t + 1))
+                    }
+                }
+            }
+            graph[Node::from(tile).id] = res;
+        }
+
+        // head straight to the goal from if within reach
+        graph[goal.id].clone().into_iter().for_each(|x| {
+            graph[x.0.id] = Edges::from_iter([(goal, x.1)]);
+        });
+
+        let adj_masks: ArrayVec<u64, MAX_NODES> = graph
+            .iter()
+            .map(|edges| edges.iter().fold(0, |mask, (adj, _)| mask | (1 << adj.id)))
+            .collect();
+
+        World {
+            graph,
+            start,
+            goal,
+            adj_masks,
+        }
     }
 
-    World { graph, goal }
+    // idea stolen from: https://github.com/mr-kaffee/aoc-2023
+    fn reachable(&self, idx: usize, seen: u64) -> u64 {
+        let mut queue = 1u64 << idx;
+        let mut reached = seen | queue;
+        while queue != 0 {
+            let cur = queue.trailing_zeros();
+            queue &= !(1 << cur);
+
+            let mask = self.adj_masks[cur as usize];
+            queue |= mask & !reached;
+            reached |= mask;
+        }
+        reached & !seen
+    }
 }
 
 fn solve(input: &str) -> (usize, usize) {
@@ -73,26 +141,17 @@ fn solve(input: &str) -> (usize, usize) {
     let size = (lines[0].len(), lines.len());
     let map = Map::new(size, lines.join("").chars());
 
-    let (start_pos, goal_pos) = (Pos::new(1, 0), Pos::new(size.0 - 2, size.1 - 1));
-    let start = Node::from(start_pos);
+    //--------------------------------------------------------------------------
+    let p1_world = World::from_map(&map, |p| make_neighbors_p1(p, &map));
+    let start = p1_world.start;
+    let mut bests = HashMap::new();
+    let p1 = compute_paths(&p1_world, start, 0, 0u64, &mut bests);
 
     //--------------------------------------------------------------------------
-    let p1_world = make_world(
-        &map,
-        (start_pos, goal_pos),
-        |p| make_neighbors_p1(p, &map),
-        |p| is_intersection(p, &map) || p == goal_pos,
-    );
-    let p1 = compute_paths(&p1_world, start, 0, &mut make_array::<MAX_NODES, bool>());
-
-    //--------------------------------------------------------------------------
-    let p2_world = make_world(
-        &map,
-        (start_pos, goal_pos),
-        |p| ArrayVec::from_iter(p.neighbors_simple()),
-        |p| is_intersection(p, &map) || p == goal_pos,
-    );
-    let p2 = compute_paths(&p2_world, start, 0, &mut make_array::<MAX_NODES, bool>());
+    let p2_world = World::from_map(&map, |p| ArrayVec::from_iter(p.neighbors_simple()));
+    let start = p2_world.start;
+    bests.clear();
+    let p2 = compute_paths(&p2_world, start, 0, 0u64, &mut bests);
 
     (p1, p2)
 }
@@ -101,57 +160,34 @@ fn compute_paths(
     world: &World,
     start: Node,
     cur_cost: usize,
-    visited: &mut ArrayVec<bool, MAX_NODES>,
+    seen: u64,
+    bests: &mut HashMap<(usize, u64), usize>,
 ) -> usize {
-    // dead-end
-    if visited[start.id] {
+    if start == world.goal {
+        return cur_cost;
+    }
+
+    // goal is not reachable
+    let reachable = world.reachable(start.id, seen);
+    if reachable & (1 << world.goal.id) == 0 {
         return 0;
     }
 
-    if let Some(reached_goal) = world.graph[start.id]
+    // idea stolen from: https://github.com/mr-kaffee/aoc-2023
+    match bests.entry((start.id, reachable)) {
+        Entry::Occupied(o) if cur_cost <= *o.get() => return 0,
+        Entry::Occupied(mut o) => *o.get_mut() = cur_cost,
+        Entry::Vacant(v) => _ = v.insert(cur_cost),
+    }
+
+    // peel off neighboring nodes and recurse
+    let new_seen = seen | 1_u64 << start.id;
+    let res = world.graph[start.id]
         .iter()
-        .find(|(n, _d)| *n == world.goal)
-    {
-        cur_cost + reached_goal.1
-    } else {
-        // peel off neighboring nodes and recurse
-        visited[start.id] = true;
-        let res = world.graph[start.id]
-            .iter()
-            .map(|&(next, cost)| compute_paths(world, next, cur_cost + cost, visited))
-            .max()
-            .unwrap();
-        visited[start.id] = false;
-        res
-    }
-}
-
-fn bfs<R: Fn(Pos) -> bool, M: Fn(Pos) -> ArrayVec<Pos, 4>>(
-    q: &mut VecDeque<(Pos, usize)>,
-    seen: &mut HashSet<Pos>,
-    make_neighbors: M,
-    is_poi: R,
-    map: &Map<char>,
-) -> Edges {
-    let mut res = Edges::new();
-
-    while let Some((cur, t)) = q.pop_front() {
-        if seen.contains(&cur) {
-            continue;
-        }
-        seen.insert(cur);
-
-        for next in make_neighbors(cur)
-            .into_iter()
-            .filter(|&p| is_valid(p, map) && !seen.contains(&p))
-        {
-            if is_poi(next) {
-                res.push((Node::from(next), t + 1));
-            } else {
-                q.push_back((next, t + 1))
-            }
-        }
-    }
+        .filter(|(n, _)| seen & 1_u64 << n.id == 0)
+        .map(|&(next, cost)| compute_paths(world, next, cur_cost + cost, new_seen, bests))
+        .max()
+        .unwrap_or(0);
     res
 }
 
